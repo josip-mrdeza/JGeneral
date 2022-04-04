b74dbd9ec3e4b099f4f7dbf6cdddf1fd5092ddd2
Josip Mrdeža <forsakennydoseth@hotmail.com>
2022-04-04 11:34:05 +0200

    Changed the Catia.cs's modifier from public to internal since the end user would have no use of the Catia Application Instance.

--- JGeneral.IO.Interop/Catia/Catia.cs
+++ JGeneral.IO.Interop/Catia/Catia.cs
@@ -2,8 +2,8 @@ using System.Runtime.InteropServices;
 
 namespace JGeneral.IO.Interop.Catia
 {
-    public static class Catia
+    internal static class Catia
     {
-        public static readonly INFITF.Application Instance = (INFITF.Application)Marshal.GetActiveObject("Catia.Application");
+        internal static readonly INFITF.Application Instance = (INFITF.Application)Marshal.GetActiveObject("Catia.Application");
     }
 }
